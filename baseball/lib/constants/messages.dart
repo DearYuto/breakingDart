d68147e617d0febfa7116aed41ab3681ead40950
DearYuto <reasons@kakao.com>
2024-04-19 23:57:52 +0900

    chore: final 키워드 const로 변경

--- baseball/lib/constants/messages.dart
+++ baseball/lib/constants/messages.dart
@@ -1,5 +1,6 @@
 class MessageConstants {
   MessageConstants._();
 
-  static final String welcome = '환영해요! 지금부터 숫자 야구 게임을 시작할게요.🥰';
+  static const String welcome = '환영해요! 지금부터 숫자 야구 게임을 시작할게요.🥰';
+  static const String inputNumbers = '3자리 숫자를 입력해주세요.';
 }
