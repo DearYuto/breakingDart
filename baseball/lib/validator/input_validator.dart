677b2be817c4240048dc8753be3921eb38c0b80a
DearYuto <reasons@kakao.com>
2024-04-20 14:58:17 +0900

    fix: validRange 메서드가 List 타입이 아닌 Iterable을 반환하여 에러나는 문제 수정

--- baseball/lib/validator/input_validator.dart
+++ baseball/lib/validator/input_validator.dart
@@ -1,17 +1,12 @@
 class InputValidator {
   InputValidator._();
 
-  // todo validator 인터페이스 만들기
   static bool validRange(String numbers) {
-    try {
-      final List<int> splittedNumbers =
-          numbers.split('').map((number) => int.parse(number));
+    final List<int> splittedNumbers =
+        numbers.split('').map((number) => int.parse(number)).toList();
 
-      return splittedNumbers.every((number) {
-        return number >= 3 && number <= 9;
-      });
-    } catch (e) {
-      throw Error();
-    }
+    return splittedNumbers.every((number) {
+      return number >= 1 && number <= 9;
+    });
   }
 }
