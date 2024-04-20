93f9405e6b6c0e1001f5c176eb72da04ec01eaf3
DearYuto <reasons@kakao.com>
2024-04-20 16:44:21 +0900

    test: StringUtils 클래스 parseToList 메서드에 옵셔널 인자로 컨버터할 타입을 리턴하는 콜백을 넣었을 때 동작 테스트

--- baseball/test/string_utils_test.dart
+++ baseball/test/string_utils_test.dart
@@ -14,5 +14,18 @@ void main() {
           ),
           result);
     });
+
+    test('문자열 123, converter 함수로 숫자를 변환하는 함수를 인자로 넣으면 숫자 1,2,3이 담긴 배열이 리턴된다.',
+        () {
+      final String input = '123';
+      final List<int> result = [1, 2, 3];
+
+      expect(
+          StringUtils.parseToList(
+            input,
+            converter: (String str) => int.parse(str),
+          ),
+          result);
+    });
   });
 }
