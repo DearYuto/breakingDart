24e6f1537664185292eff7e0cfff8845f55948ce
DearYuto <reasons@kakao.com>
2024-04-21 18:40:00 +0900

    test: inputValidator 클래스 테스트 코드 수정

--- baseball/test/input_validator_test.dart
+++ baseball/test/input_validator_test.dart
@@ -5,16 +5,23 @@ import 'package:test/scaffolding.dart';
 
 void main() {
   group('inputValidator 클래스를 테스트할게요. 🐈', () {
+    final bool Function(int) callback =
+        (int num) => num >= GameRules.MIN_NUM && num <= GameRules.MAX_NUM;
+
     test(
         '입력 받은 값이 ${GameRules.MIN_NUM} ~ ${GameRules.MAX_NUM} 사이의 숫자로 이루어져 있으면 true를 반환해요.',
         () {
-      expect(InputValidator.validRange('123'), isTrue);
+      const List<int> inputs = [1, 2, 3];
+
+      expect(InputValidator.validRange(inputs, callback), isTrue);
     });
 
     test(
         '입력 값이 ${GameRules.MIN_NUM} ~ ${GameRules.MAX_NUM} 사이가 아니면 false를 반환해요.',
         () {
-      expect(InputValidator.validRange('000'), isFalse);
+      const List<int> inputs = [0, 0, 0];
+
+      expect(InputValidator.validRange(inputs, callback), isFalse);
     });
   });
 }
