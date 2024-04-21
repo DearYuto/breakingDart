6fad301a1186dc13fef4c9a7f7bef55ab3f3c882
DearYuto <reasons@kakao.com>
2024-04-21 18:49:02 +0900

    feat: 사용자의 인풋 유효성을 처리할 InputValidator 클래스 추가

--- baseball/lib/validator/input_validator.dart
+++ baseball/lib/validator/input_validator.dart
@@ -1,14 +1,7 @@
-import '../constants/game_rules.dart';
-
 class InputValidator {
   InputValidator._();
 
-  static bool validRange(String numbers) {
-    final List<int> splittedNumbers =
-        numbers.split('').map((number) => int.parse(number)).toList();
-
-    return splittedNumbers.every((number) {
-      return number >= GameRules.MIN_NUM && number <= GameRules.MAX_NUM;
-    });
+  static bool validRange<T>(List<T> inputs, bool Function(T) conditionCb) {
+    return inputs.every(conditionCb);
   }
 }
