b5c531986dd0e32e8f0f0a553841d52aec5e5725
DearYuto <reasons@kakao.com>
2024-04-21 23:24:51 +0900

    feat(InputValidator): 인수로 받은 List의 중복 값을 제거할 경우 처음 인수와 길이가 같은지 확인하는 메서드 추가

--- baseball/lib/validator/input_validator.dart
+++ baseball/lib/validator/input_validator.dart
@@ -1,7 +1,15 @@
+import 'package:baseball/constants/game_rules.dart';
+
 class InputValidator {
   InputValidator._();
 
   static bool validRange<T>(List<T> inputs, bool Function(T) conditionCb) {
     return inputs.every(conditionCb);
+  }
+
+  static bool isUniqueList<T>(List<T> inputs) {
+    Set<T> inputSet = Set<T>.from(inputs);
+
+    return inputSet.length == inputs.length;
   }
 }
