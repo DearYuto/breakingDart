84c5af8848c06c17dd9246de3db4eb24b63d39db
DearYuto <reasons@kakao.com>
2024-04-21 21:16:04 +0900

    feat: InputView, OutputView class 추가

---
+++ baseball/lib/view/output_view.dart
@@ -0,0 +1,11 @@
+import '../constants/messages.dart';
+
+class OutputView {
+  void printWelcome() {
+    print(MessageConstants.welcome);
+  }
+
+  void printInputNumbers() {
+    print(MessageConstants.inputNumbers);
+  }
+}
