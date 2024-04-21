a510e5eb661446649fb6f3ca126b320a14cdb51b
DearYuto <reasons@kakao.com>
2024-04-21 18:29:34 +0900

    refactor: parseToList 메서드 재사용할 수 있도록 로직 개선, 컨버터 콜백을 넘겨주지 않은 경우 ArgumentError throw 하도록 변경

--- baseball/lib/utils/string_utils.dart
+++ baseball/lib/utils/string_utils.dart
@@ -1,9 +1,17 @@
+import 'package:baseball/constants/error_messages.dart';
+
 class StringUtils {
   StringUtils._();
 
-  static List<T> parseToList<T>(String str, {T Function(String)? converter}) {
-    converter ??= (s) => s as T;
+  // todo StringUtils 네이밍이 뭔가 애매해서 고민좀 해바야할듯,,
 
-    return str.split('').map((char) => converter!(char)).toList();
+  static List<T> parseToList<T>(String str, {T Function(String)? converter}) {
+    if (converter == null) {
+      if (T == String) return str.split('').cast<T>().toList();
+
+      throw ArgumentError(ErrorMessageConstants.invalidConvertType);
+    }
+
+    return str.split('').map((char) => converter(char)).toList();
   }
 }
