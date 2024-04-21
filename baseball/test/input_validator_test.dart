fc02e89442e28f965a0bcfd5bf0a1dbee60063f8
DearYuto <reasons@kakao.com>
2024-04-21 23:23:59 +0900

    test: InputValidator 클래스 isUniqueList 메서드 테스트(인풋으로 넣은 List가 중복 값을 제거 했을 때 처음에 넣은 리스트와 길이가 동일한지 테스트)

--- baseball/test/input_validator_test.dart
+++ baseball/test/input_validator_test.dart
@@ -5,40 +5,77 @@ import 'package:test/scaffolding.dart';
 
 void main() {
   group('inputValidator 클래스를 테스트할게요. 🐈', () {
-    final bool Function(int) callback =
-        (int num) => num >= GameRules.minNum && num <= GameRules.maxNum;
+    group('✅ validRange 메서드 테스트', () {
+      final bool Function(int) callback =
+          (int num) => num >= GameRules.minNum && num <= GameRules.maxNum;
 
-    final testCases = [
-      {
-        'input': [1, 2, 3],
-        'expected': true
-      },
-      {
-        'input': [0, 0, 0],
-        'expected': false,
-      },
-      {
-        'input': [5, 1, 8],
-        'expected': true,
-      },
-      {
-        'input': [4, 10, 400],
-        'expected': false,
-      },
-      {
-        'input': [4, 5, -4],
-        'expected': false,
-      },
-    ];
+      const List<Map<String, dynamic>> testCases = [
+        {
+          'input': [1, 2, 3],
+          'expected': true
+        },
+        {
+          'input': [0, 0, 0],
+          'expected': false,
+        },
+        {
+          'input': [5, 1, 8],
+          'expected': true,
+        },
+        {
+          'input': [4, 10, 400],
+          'expected': false,
+        },
+        {
+          'input': [4, 5, -4],
+          'expected': false,
+        },
+      ];
 
-    for (var testCase in testCases) {
-      test(
-          '입력 받은 값: ${testCase['input']}, ${GameRules.minNum} ~ ${GameRules.maxNum} 사이의 숫자로 이루어져 있으면 ${testCase['expected']}를 반환해요.',
-          () {
-        expect(
-            InputValidator.validRange(testCase['input'] as List<int>, callback),
-            testCase['expected']);
+      for (var testCase in testCases) {
+        test(
+            '입력 받은 값: ${testCase['input']}, ${GameRules.minNum} ~ ${GameRules.maxNum} 사이의 숫자로 이루어져 있으면 ${testCase['expected']}를 반환해요.',
+            () {
+          expect(
+              InputValidator.validRange(
+                  testCase['input'] as List<int>, callback),
+              testCase['expected']);
+        });
+      }
+    });
+
+    group('✅ isUniqueList 메서드 테스트', () {
+      const List<Map<String, dynamic>> testCases = [
+        {
+          'list': [1, 2, 3],
+          'setList': [1, 2, 3],
+          'expected': true,
+        },
+        {
+          'list': [1, 1, 1],
+          'setList': [1],
+          'expected': false,
+        },
+        {
+          'list': [1],
+          'setList': [1],
+          'expected': true,
+        },
+        {
+          'list': [1, 2],
+          'setList': [1, 2],
+          'expected': true,
+        }
+      ];
+
+      testCases.forEach((element) {
+        test(
+            'list: ${element['list']}, setList: ${element['setList']}, 중복 값을 제거했을 때 길이는 ${element['setList'].length}, 리턴 결과는 ${element['expected']}여야 해요.',
+            () {
+          expect(InputValidator.isUniqueList(element['list']),
+              element['list'].length == element['setList'].length);
+        });
       });
-    }
+    });
   });
 }
