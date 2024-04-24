992a69b2e75cab3059653be3c620e37cfad5a07c
DearYuto <reasons@kakao.com>
2024-04-24 23:14:36 +0900

    feat(GameResult): 게임 결과 model 추가

---
+++ baseball/lib/model/game_result.dart
@@ -0,0 +1,11 @@
+class GameResult {
+  final int strike;
+  final int ball;
+  final bool nothing;
+
+  GameResult({
+    required this.strike,
+    required this.ball,
+    required this.nothing,
+  });
+}
