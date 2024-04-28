33c8355f74f9543845c38bff7f4f88bc816c5234
DearYuto <reasons@kakao.com>
2024-04-28 18:40:35 +0900

    feat(InputView): 사용자로부터 인풋을 받을 때 메시지 출력 후 인풋을 받을 수 있는 메서드 추가

--- baseball/lib/view/input_view.dart
+++ baseball/lib/view/input_view.dart
@@ -4,4 +4,9 @@ class InputView {
   String readInput() {
     return stdin.readLineSync() ?? '';
   }
+
+  String readMessageInput(String message) {
+    stdout.write('$message\n');
+    return readInput();
+  }
 }
