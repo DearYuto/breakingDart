7df8b30f34762ff4ae08a90be169cb0f03ef56cc
DearYuto <reasons@kakao.com>
2024-08-17 19:09:38 +0900

    refactor: 출력 메시지 OutputView 클래스 역할로 변경

---
+++ calculator/lib/view/output_view.dart
@@ -0,0 +1,9 @@
+class OutputView {
+  static void printStart() {
+    print('계산기 프로그램을 실행합니다.');
+  }
+
+  static void printFirstNumberInput() {
+    print('연산할 첫 번째 숫자를 입력해주세요.');
+  }
+}
--- calculator/main.dart
+++ calculator/main.dart
@@ -1,40 +1,28 @@
 import 'dart:io';
 
-void main() {
-  print('계산기 프로그램을 실행합니다.');
-  print('연산할 첫 번째 숫자를 입력해주세요.');
+import 'package:calculator/view/output_view.dart';
 
-  final double num1 = getUserInput();
+void main() {
+  OutputView.printStart();
+  OutputView.printFirstNumberInput();
+
+  // final double num1 = getUserInput();
   // final num2 = getUserInput();
 }
 
-void validateInput(String? userInput) {
-  if (userInput == null || userInput.isEmpty) {
-    throw Exception('잘못된 입력값입니다.');
-  }
+// double getUserInput() {
+//   String? input;
 
-  if (double.tryParse(userInput) == null) {
-    throw Exception('빈 값은 입력할 수 없습니다.');
-  }
-}
+//   while (true) {
+//     try {
+//       input = readUserInput();
+//       validateInput(input);
+//       break;
+//     } catch (err) {
+//       print(err);
+//       input = readUserInput();
+//     }
+//   }
 
-String? readUserInput() {
-  return stdin.readLineSync();
-}
-
-double getUserInput() {
-  String? input;
-
-  while (true) {
-    try {
-      input = readUserInput();
-      validateInput(input);
-      break;
-    } catch (err) {
-      print(err);
-      input = readUserInput();
-    }
-  }
-
-  return double.parse(input!);
-}
+//   return double.parse(input!);
+// }
