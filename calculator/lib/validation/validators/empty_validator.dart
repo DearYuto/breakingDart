2740a71a153b74f51f1a69d2c6ae9faac46c4c7c
DearYuto <reasons@kakao.com>
2024-08-17 19:04:09 +0900

    feat(EmptyValidator): 빈 입력 값에 대한 검증을 처리하는 클래스 정의

---
+++ calculator/lib/validation/validators/empty_validator.dart
@@ -0,0 +1,11 @@
+import 'package:calculator/validation/validators/abstract_validator.dart';
+
+class EmptyValidator extends AbstractValidator<String> {
+  @override
+  ValidationResult validate(String? value) {
+    if (value == null || value.isEmpty) {
+      return ValidationResult(false, '빈 값은 입력할 수 없습니다.');
+    }
+    return ValidationResult(true);
+  }
+}
